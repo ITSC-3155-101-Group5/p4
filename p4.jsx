9fa31973802a596cf404a1e8c5476568b669c3a7
Nick <nick@Owners-MacBook-Air.local>
2026-03-08 21:47:23 -0400

    add dynamic view v1

---
+++ p4.jsx
@@ -0,0 +1,53 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header/Header';
+import Example from './components/example/Example';
+import States from './components/states/States';
+
+class P4 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentView: 'example',
+    };
+
+    this.handleSwitchView = this.handleSwitchView.bind(this);
+  }
+
+  handleSwitchView() {
+    this.setState((prevState) => ({
+      currentView: prevState.currentView === 'example' ? 'states' : 'example',
+    }));
+  }
+
+  render() {
+    const { currentView } = this.state;
+
+    const buttonText =
+      currentView === 'example'
+        ? 'Switch to States'
+        : 'Switch to Example';
+
+    return (
+      <div>
+        <Header />
+
+        <div className="view-switcher">
+          <button type="button" onClick={this.handleSwitchView}>
+            {buttonText}
+          </button>
+        </div>
+
+        <div className="view-container">
+          {currentView === 'example' ? <Example /> : <States />}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<P4 />, document.getElementById('reactapp'));
