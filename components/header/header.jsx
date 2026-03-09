1faf8974c2c898b80b52cb63a2cc0f3203b98ff0
Elijah James <ejames37@charlotte.edu>
2026-03-08 22:13:45 -0400

    Create header.jsx

---
+++ components/header/header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header-container">
+
+        <div className="header-left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+            alt="logo"
+            className="header-logo"
+          />
+        </div>
+
+        <div className="header-center">
+          <h1>Elijah's React Projects</h1>
+          <p className="header-subtitle">Building cool web apps</p>
+        </div>
+
+        <div className="header-right">
+          <span>ITSC 3155</span>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Header;
