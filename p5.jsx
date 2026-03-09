f5592811442e3f1e1ef7acb49dc34a6b41c29557
Nick <nick@Owners-MacBook-Air.local>
2026-03-08 22:41:14 -0400

    done p5

---
+++ p5.jsx
@@ -0,0 +1,33 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Example from './components/example/Example';
+import States from './components/states/States';
+
+class P5 extends React.Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <Header />
+
+          <nav className="toolbar">
+            <Link to="/example" className="toolbar-link">Example</Link>
+            <Link to="/states" className="toolbar-link">States</Link>
+          </nav>
+
+          <div className="view-container">
+            <Route path="/example" component={Example} />
+            <Route path="/states" component={States} />
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+ReactDOM.render(<P5 />, document.getElementById('reactapp'));
